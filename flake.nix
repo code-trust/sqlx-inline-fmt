735f1e59ca2095f24653e1a83bcd8ff552e3ce5a
Lauri Niskanen <ape@ape3000.com>
2025-10-30 12:24:34 +0200

    Bump version to 0.2.0

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
 
         packages.default = pkgs.rustPlatform.buildRustPackage rec {
           pname = "sqlx-inline-fmt";
-          version = "0.1.0";
+          version = "0.2.0";
           src = lib.cleanSource ./.;
 
           cargoLock = {
